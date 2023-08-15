a7e4a8cd080e6198940d6407e6e24bae8b79b467
NomadesPrime <fernandesrichard312@gmail.com>
2023-08-15 12:37:56 -0300

    desafio

--- Exercicio2/Desafio.java
+++ Exercicio2/Desafio.java
@@ -2,5 +2,34 @@ package Exercicio2;
 import java.util.Scanner;
 
 public class Desafio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor pago: ");
+        int valorPago = sc.nextInt();
+        int[] notas = {100, 50, 10, 5, 1};
+        int[] quantidadeNotas = new int[5];
+        int i = 0;
+        while (valorPago > 0) {
+            if (valorPago >= notas[i]) {
+                valorPago -= notas[i];
+                quantidadeNotas[i]++;
+            } else {
+                i++;
+            }
+        }
+        System.out.println("Troco que deve ser dado: ");
+        for (int j = 0; j < notas.length; j++) {
+          if (quantidadeNotas[j] == 1){
+                System.out.println(quantidadeNotas[j] + " nota de " + notas[j]);
+          }
+            else if (quantidadeNotas[j] > 1){
+                System.out.println(quantidadeNotas[j] + " notas de " + notas[j]);
+
+          }
+        }
+
+
+
+    }
 
 }
